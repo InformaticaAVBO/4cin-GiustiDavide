34b5d0c09152b4516dc50cddd287b0ddcbfc67b5
GiustiDavide <davide.giusti@aldini.istruzioneer.it>
2026-04-22 09:40:49 +0000

    uuiuiuygghjhjjhjgjhhvhgjhjvhgvjhvjhvjhgvjhv

---
+++ Tree/src/Nodo.java
@@ -0,0 +1,38 @@
+public class Nodo<T> {
+    private T info;
+    private Nodo<T> left, right;
+
+    public Nodo(T info) {
+        this.info = info;
+        this.left = null;
+        this.right = null;
+    }
+
+    public T getInfo() {
+        return info;
+    }
+
+    public void setInfo(T info) {
+        this.info = info;
+    }
+
+    public Nodo<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Nodo<T> left) {
+        this.left = left;
+    }
+
+    public Nodo<T> getRight() {
+        return right;
+    }
+
+    public void setRight(Nodo<T> right) {
+        this.right = right;
+    }
+
+    public String toString() {
+        return info.toString();
+    }
+}
---
+++ Tree/src/Tree.java
@@ -0,0 +1,17 @@
+public class Tree {
+    private Nodo<T> root;
+
+    public Tree() {
+        this.root = null;
+    }
+
+    public void add(T info) {
+        Nodo<T> newNode = new Nodo<>(info);
+        if (root == null) {
+            root = newNode;
+        } else {
+            newNode.setRight(root);
+            root = newNode;     
+        }
+    }
+}
